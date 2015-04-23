612a758ab42e77b644f749829f19cf90d1f50dbf
msmalley <mark@smalley.my>
2015-04-23 11:15:51 +0800

    now has a content section that is driven from the same README as GitHub

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ Using - http://blockstrap.com
 */
 
 error_reporting(-1);
+
 $php_base = dirname(__FILE__);
+$docs = file_get_contents($php_base.'/README.md');
 $template = file_get_contents($php_base.'/html/index.html');
 $options = json_decode(file_get_contents($php_base.'/json/index.json'), true);
 
@@ -16,9 +18,14 @@ include_once($php_base.'/php/blockauth.php');
 include_once($php_base.'/php/mustache.php');
 include_once($php_base.'/php/parsedown.php');
 
+$md = new Parsedown();
 $auth = new blockauth();
+
 $options['auth'] = $auth->user();
 $options['vars'] = $auth::$vars;
+
+$documentation = $md->parse($docs);
+$options['docs'] = $documentation;
 
 $mustache = new MustachePHP();
 $html = $mustache->render($template, $options);
